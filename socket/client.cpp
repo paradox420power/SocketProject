3fa2022ee873e862cc94c436d8c3a771ce7365fd
paradox420power <yodapowr@gmail.com>
2019-02-10 09:14:34 -0700

    Add files via upload
    Updated Server to handle clients in threads instead of forks to address issue where clients couldn't see changes to the player list after registering. Implemented "Query Players" command. Players must now register before any other command & can only register once.

--- socket/client.cpp
+++ socket/client.cpp
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <string.h>
 #include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
 #include <unistd.h>
 #include <iostream>
 
@@ -13,10 +16,10 @@
 
 using namespace std;
 
-struct message{
-	string command; //Register, Query, Start, Query, End, Deregister
-	string value; //<Player Name>, Players, Game, Games (determined by command)
-	string numeric; //IP Addr., Player Count, Game ID (determined by command)
+struct player{
+	string name;
+	string IP;
+	string port;
 };
 
 void DieWithError(const char *errorMessage){ //included in sample code, carried over to project
@@ -26,17 +29,166 @@ void DieWithError(const char *errorMessage){ //included in sample code, carried 
 
 void clientInput(FILE *fp, int sockfd){
 	ssize_t n;
-    char    sendline[ECHOMAX], recvline[ECHOMAX];
+    char sendline[ECHOMAX], recvline[ECHOMAX];
 	
+	//know what command was sent & what return to expect
+	string command = ""; //Register, Query, Start, Query, End, Deregister
+	string value = "";
+	string IP = "";
+	string port = "";
+	string output;
+	string message;
+	bool validInput;
+	
+	bool unreg = true;
+	cout << "Please register: (formet Register <name> <IP> <port>" << endl;
+	while(unreg && fgets(sendline, ECHOMAX, fp) != NULL){ //after connecting, register
+		string message = string(sendline); //convert char message into string
+		command = ""; //reset fields
+		value = "";
+		int count = 0;
+		istringstream iss(message); //delimit spaces from the message
+		do{
+			string part;
+			iss >> part;
+			switch(count){
+				case 0: command = part;
+					break;
+				case 1: value = part;
+					break;
+				case 2: IP = part;
+					break;
+				case 3: port = part;
+					break;
+				default: //do nothing
+					break;
+			}
+			count++;
+		}while(iss);
+		
+		if(command.compare("register") == 0 || command.compare("Register") == 0){ //Register Player IP
+			//add player to list
+			validInput = true;
+			if(value.compare("") != 0 && IP.compare("") != 0 && port.compare("") != 0){ //ensure all data was passed to server
+				write(sockfd, sendline, strlen(sendline));
+				//make sure server completed successfully
+				memset(recvline, 0 ,255);
+				if ( (n = read(sockfd, recvline, ECHOMAX) == 0) )
+					DieWithError("str_cli: server terminated prematurely");
+				string output = string(recvline);
+				istringstream buf(output);
+				int successField;
+				buf >> successField;
+				cout << successField << endl;
+				if(successField == 0){
+					cout << "Registered" << endl;
+					unreg = false;
+				}else{
+					cout << "Failed to Register" << endl;
+				}
+			}else{
+				cout << "Not all fields full" << endl;
+			}
+		}else{
+			cout << "Incorrect command issued" << endl;
+		}
+	}
+	
+	//continuously loop
 	while (fgets(sendline, ECHOMAX, fp) != NULL) {
+		
+		//translate snet command to anticipate return
+		string message = string(sendline); //convert char message into string
+		command = ""; //reset fields
+		value = "";
+		int count = 0;
+		istringstream iss(message); //delimit spaces from the message
+		do{
+			string part;
+			iss >> part;
+			switch(count){
+				case 0: command = part;
+					break;
+				case 1: value = part;
+					break;
+				default: //do nothing
+					break;
+			}
+			count++;
+		}while(iss);
+		
+		//send a command to the server
+		if(command.compare("register") != 0 && command.compare("Register") != 0){ //don't allow 1 client to register twice
+			write(sockfd, sendline, strlen(sendline));
+			if ( (n = read(sockfd, recvline, ECHOMAX) == 0) )
+				DieWithError("str_cli: server terminated prematurely");
+			output = string(recvline);
+		}
+		
+		//interpret the command
+		validInput = false;
+		if(command.compare("register") == 0 || command.compare("Register") == 0){ //Register Player IP
+			//add player to list
+			cout << "You are already registered" << endl;
 			
-        write(sockfd, sendline, strlen(sendline));
-
-        if ( (n = read(sockfd, recvline, ECHOMAX)) == 0)
-            DieWithError("str_cli: server terminated prematurely");
-
-		recvline[ n ] = '\0';
-        fputs(recvline, stdout);
+		}else if(command.compare("query") == 0 || command.compare("Query") == 0){ //Query Players or Games
+			if(value.compare("players") == 0 || value.compare("Players") == 0){
+				//expect list of players
+				vector<string> triples;
+				istringstream buffer(output);
+				string part;
+				while(getline(buffer, part, ' ')){
+					triples.push_back(part);
+				}
+				int playerCount; //get an expected player count output
+				istringstream convert(triples[0]); //first value will be palyer count
+				convert >> playerCount;
+				triples.erase(triples.begin()); //remove head
+				int readPlayers = 0;
+				while(!triples.empty()){ //read anything else in the buffer
+					cout << "Player " << triples[0] << " at IP " << triples[1] << " and port " << triples[2] << endl;
+					readPlayers++; //increment the player count
+					triples.erase(triples.begin(), triples.begin()+3); //erase the read palyer
+				}
+				while(readPlayers < playerCount){ //repeat above for all players
+					memset(recvline, 0, 255); //clean old entries in buffer
+					if ( (n = read(sockfd, recvline, ECHOMAX) == 0) ) //rea anything in the buffer
+						DieWithError("str_cli: server terminated prematurely");
+					output = string(recvline);
+					istringstream buffer(output);
+					string part;
+					while(getline(buffer, part, ' ')){ //break inputs by spaace & push to vector
+						triples.push_back(part);
+					}
+					while(!triples.empty()){ //read triplets from vector & print to screen
+						cout << "Player " << triples[0] << " at IP " << triples[1] << " and port " << triples[2] << endl;
+						readPlayers++; //incremnet read player count
+						triples.erase(triples.begin(), triples.begin()+3); //erase read palyers from vector
+					}
+				}
+				
+			}else if(value.compare("games") == 0 || value.compare("Games") == 0){
+				//expect list of games
+				cout << output << endl;
+			}
+			
+		}else if(command.compare("start") == 0 || command.compare("Start") == 0){ //Start Game k
+			//handle new game
+			cout << output << endl;
+			
+		}else if(command.compare("end") == 0 || command.compare("End") == 0){ //End <Game ID>
+			//end any client side game attributes
+			cout << output << endl;
+			
+		}else if(command.compare("deregister") == 0 || command.compare("Deregister") == 0){ //Deregister Player
+			//remove self, expected to end client soon after
+			cout << output << endl;
+		}else{
+			cout << "Command unrecognized: " << output << endl;
+		}
+		//cout << "End process" << endl;
+		memset(recvline, 0, 255); //clean buffer before receiving new input
+		
     }
 }
 
--- socket/server.cpp
+++ socket/server.cpp
@@ -1,3 +1,4 @@
+//compiled as "g++ -o server -pthread server.cpp"
 #include <sys/socket.h> 	/* for socket() and bind() */
 #include <stdio.h>		/* printf() and fprintf() */
 #include <stdlib.h>		/* for atoi() and exit() */
@@ -7,25 +8,112 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 #define	MES_MAX	255		/* Longest string to echo */
 #define BACKLOG	128
 
 using namespace std;
 
+struct player{
+	string name;
+	string IP;
+	string port;
+};
+static vector<player> playerList; //vector to track all registered players
+pthread_mutex_t pListUpdate = PTHREAD_MUTEX_INITIALIZER; //only 1 thread should be modifying the List at a time
+
+struct ThreadArgs{
+	int sockfd;
+};
+
+int getPlayerIndex(string pName){
+	int index = -1;
+	for(int x = 0; x < playerList.size(); x++){
+		if(playerList[x].name.compare(pName) == 0)
+			index = x;
+	}
+	return index;
+}
+
 void DieWithError(const char *errorMessage){//included in sample code, carried over to project
 	perror(errorMessage);
 	exit(1);
 }
 
-void serverInterface(int sockfd){
-    ssize_t n;
+int registerPlayer(int sockfd){
+	ssize_t n;
+    char    line[MES_MAX];
+	string command = ""; //Register, Query, Start, Query, End, Deregister
+	string name = ""; //<Player Name>, Players, Game, Games (determined by command)
+	string IP = ""; //IP Addr., Player Count, Game ID (determined by command)
+	string port = ""; //only used in register command
+	bool unreg = true;
+	bool stillConnected = true;
+	int isReg = 0;
+	
+	while(unreg && stillConnected){
+		if ( (n = read(sockfd, line, MES_MAX)) == 0 )
+			stillConnected = false; //client immediately disconnects
+		
+		string message = string(line); //convert char message into string
+		int count = 0;
+		istringstream iss(message); //delimit spaces from the message
+		do{
+			string part;
+			iss >> part;
+			switch(count){
+				case 0: command = part;
+					break;
+				case 1: name = part;
+					break;
+				case 2: IP = part;
+					break;
+				case 3: port = part;
+				default: //do nothing
+					break;
+			}
+			count++;
+		}while(iss);
+		//interpret the command
+		if(command.compare("register") == 0 || command.compare("Register") == 0){ //Register Player IP
+			//add player to list
+			if(name.compare("") != 0 && IP.compare("") != 0 && port.compare("") != 0){ //ensure all data was passed to server
+				if(getPlayerIndex(name) == -1){ //don't allow duplicate player names
+					playerList.push_back(player());
+					playerList[playerList.size()-1].name = name;
+					playerList[playerList.size()-1].IP = IP;
+					playerList[playerList.size()-1].port = port;
+					write(sockfd, "0", 1 ); //return success
+					unreg = false;
+					isReg = 1;
+				}else{
+					write(sockfd, "1", 1 ); //return failure
+				}
+			}else{
+				write(sockfd, "1", 18); //return failure
+			}
+			
+		}
+	}
+	return isReg; //0 if they disconnect, 1 if they register correctly	
+}
+
+void *serverInterface(void *threadArgs){
+    
+	int sockfd;
+	pthread_detach(pthread_self());
+	sockfd = ((struct ThreadArgs *) threadArgs ) -> sockfd;
+	free(threadArgs);
+	ssize_t n;
     char    line[MES_MAX];
 	string command = ""; //Register, Query, Start, Query, End, Deregister
 	string value = ""; //<Player Name>, Players, Game, Games (determined by command)
 	string numeric = ""; //IP Addr., Player Count, Game ID (determined by command)
+	string port = ""; //only used in register command
 	bool validInput = false;
 	
 	printf("Handling %d\n", sockfd); //debug info
@@ -36,9 +124,10 @@ void serverInterface(int sockfd){
 	while(stillConnected){
 		
 		string message = string(line); //convert char message into string
-		string command = ""; 
-		string value = "";
-		string numeric = "";
+		command = ""; //reset fields
+		value = "";
+		numeric = "";
+		port = "";
 		int count = 0;
 		istringstream iss(message); //delimit spaces from the message
 		do{
@@ -51,56 +140,72 @@ void serverInterface(int sockfd){
 					break;
 				case 2: numeric = part;
 					break;
+				case 3: port = part;
 				default: //do nothing
 					break;
 			}
 			count++;
 		}while(iss);
 		
-		cout << command << " " << value << " " << numeric << endl;
+		cout << command << " " << value << " " << numeric << " " << port << endl;
 		
 		//interpret the command
 		validInput = false;
-		if(command.compare("register") == 0 || command.compare("Register") == 0){ //Register Player IP
-			//add player to list
-			write(sockfd, "Player Added\n", 13 );
-			validInput = true;
-		}else if(command.compare("query") == 0 || command.compare("Query") == 0){ //Query Players or Games
+		//if(command.compare("register") == 0 || command.compare("Register") == 0){ //everyone should register before entering this function
+		if(command.compare("query") == 0 || command.compare("Query") == 0){ //Query Players or Games
+			
 			if(value.compare("players") == 0 || value.compare("Players") == 0){
-				write(sockfd, "ask player\n", 11 );
+				stringstream ss;
+				ss << playerList.size();
+				string playerCount = ss.str() + " ";
+				write(sockfd, playerCount.c_str(), playerCount.size());
+				
+				for(int x = 0; x < playerList.size(); x++){
+					string toSend = playerList[x].name + " " + playerList[x].IP + " " + playerList[x].port + " "; //end with space to ensure delimiting
+					write(sockfd, toSend.c_str(), toSend.size());
+				}
 				validInput = true;
+				
 			}else if(value.compare("games") == 0 || value.compare("Games") == 0){
-				write(sockfd, "ask games\n", 10 );
+				write(sockfd, "ask games", 9 );
 				validInput = true;
 			}
+			
 		}else if(command.compare("start") == 0 || command.compare("Start") == 0){ //Start Game k
 			if(value.compare("game") == 0 || value.compare("Game") == 0){
 				int playerCount;
 				istringstream buffer(numeric);
 				iss >> playerCount;
 				//get int & start game
-				write(sockfd, "Start Game\n", 12 );
+				write(sockfd, "Start Game", 11 );
 				validInput = true;
 			}
+			
 		}else if(command.compare("end") == 0 || command.compare("End") == 0){ //End <Game ID>
 			//search & remove ID from list
-			write(sockfd, "End Game\n", 9 );
+			write(sockfd, "End Game", 8 );
 			validInput = true;
+			
 		}else if(command.compare("deregister") == 0 || command.compare("Deregister") == 0){ //Deregister Player
 			//search & remove player from list
-			write(sockfd, "Drop Player\n", 12 );
+			pthread_mutex_lock(&pListUpdate); //don't execute if someon else is (de)registering
+			//TODO
+			pthread_mutex_unlock(&pListUpdate);
+			write(sockfd, "Drop Player", 11 );
 			validInput = true;
 		}
 		
 		if(!validInput){
-			write(sockfd, "Input not recognized\n", 32 );
+			write(sockfd, "Input not recognized", 31 );
 			validInput = true;
 		}
-			
+		
+		cout << "End process" << endl;
+		memset(line, 0, 255); //clean out the buffer before recievving next input
 		if ( (n = read(sockfd, line, MES_MAX)) == 0 ) //read next command
    	    	stillConnected = false; /* connection closed by other end */
 	}
-	return; //no return value
+	//return; //no return value
 	
 }
 
@@ -133,17 +238,29 @@ int main(int argc, char **argv){
   
 	if (listen(sock, BACKLOG) < 0 )
 		DieWithError("server: listen() failed");
-		
+	
+	int isReg;
+	pthread_t threadID;
+	struct ThreadArgs *threadArgs;
+	
 	for(;;){ //loop the server indefinitely
 		cliAddrLen = sizeof(echoClntAddr);
 		connfd = accept( sock, (struct sockaddr *) &echoClntAddr, &cliAddrLen );
 		
-		int processID = fork();
-		if(processID == 0){ //create a child thread for new client
+		pthread_mutex_lock(&pListUpdate); //lock player list before modifying it
+		isReg = registerPlayer(connfd); //register before forking off
+		pthread_mutex_unlock(&pListUpdate); //unlock for further editing
+		
+		if(isReg == 1){
+			
 			printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
-			serverInterface(connfd);
-			printf("Connection Closed\n");
+			threadArgs = (struct ThreadArgs *)malloc(sizeof(struct ThreadArgs));
+			threadArgs -> sockfd = connfd;
+			pthread_create(&threadID, NULL, serverInterface, (void *) threadArgs);
+			
+		}else{ //they disconnected very early
 			close(connfd);
+			cout << "Client failed to Regsiter" << endl;
 		}
 	}
 }
